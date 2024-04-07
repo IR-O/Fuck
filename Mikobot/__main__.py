932ca9590ce534e13f42b2472c30b959c42cc933
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-04-07 06:56:02 -0700

    Update __main__.py

--- Mikobot/__main__.py
+++ Mikobot/__main__.py
@@ -258,7 +258,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await guu.delete()  # Await this line
             await message.reply_photo(
                 photo=str(choice(START_IMG)),
-                caption=PM_START_TEXT.format(escape_markdown(first_name)),                
+                PM_START_TEXT.format(escape_markdown(first_name)),                
                 reply_markup=InlineKeyboardMarkup(START_BTN),
                 parse_mode=ParseMode.MARKDOWN,
             )
