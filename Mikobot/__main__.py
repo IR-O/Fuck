54a0220971eae66039ffe11e552c3505ace3e2fa
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2024-04-07 07:07:07 -0700

    Update __main__.py

--- Mikobot/__main__.py
+++ Mikobot/__main__.py
@@ -256,12 +256,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             guu = await update.effective_message.reply_text("✨")
             await asyncio.sleep(1.8)
             await guu.delete()  # Await this line
-            await message.reply_photo(
+            await update.effective_message.reply_photo(
                 photo=str(choice(START_IMG)),
                 PM_START_TEXT.format(escape_markdown(first_name)),                
                 reply_markup=InlineKeyboardMarkup(START_BTN),
                 parse_mode=ParseMode.MARKDOWN,
-           )
+            )
     else:
         await message.reply_photo(
             photo=str(choice(START_IMG)),
